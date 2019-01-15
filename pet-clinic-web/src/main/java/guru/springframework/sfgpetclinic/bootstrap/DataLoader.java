019e44abdaaef2aabb6b1e1e4121595892be016a
Dave Weernink <DavesRepository@users.noreply.github.com>
2019-01-15 18:52:57 +0100

    added PetTypes to bootstrap. closes #32

--- pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/bootstrap/DataLoader.java
@@ -1,8 +1,10 @@
 package guru.springframework.sfgpetclinic.bootstrap;
 
 import guru.springframework.sfgpetclinic.model.Owner;
+import guru.springframework.sfgpetclinic.model.PetType;
 import guru.springframework.sfgpetclinic.model.Vet;
 import guru.springframework.sfgpetclinic.services.OwnerService;
+import guru.springframework.sfgpetclinic.services.PetTypeService;
 import guru.springframework.sfgpetclinic.services.VetService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
@@ -12,14 +14,19 @@ public class DataLoader implements CommandLineRunner {
 
   private final OwnerService ownerService;
   private final VetService vetService;
+  private final PetTypeService petTypeService;
 
-  public DataLoader(OwnerService ownerService, VetService vetService) {
+  public DataLoader(OwnerService ownerService, VetService vetService, PetTypeService petTypeService) {
     this.ownerService = ownerService;
     this.vetService = vetService;
+    this.petTypeService = petTypeService;
   }
 
   @Override
   public void run(String... args) {
+    final PetType dog = createPetType("Dog");
+    final PetType cat = createPetType("Cat");
+
     createOwner("Michael", "Weston");
     createOwner("Fiona", "Glenanne");
 
@@ -29,6 +36,12 @@ public class DataLoader implements CommandLineRunner {
     createVet("John", "Smith");
 
     System.out.println("loaded vets....");
+  }
+
+  private PetType createPetType(String dog) {
+    final PetType petType = new PetType();
+    petType.setName(dog);
+    return petTypeService.save(petType);
   }
 
   private void createOwner(String firstName, String lastName) {
